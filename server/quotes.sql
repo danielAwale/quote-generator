f29f76678c61d317a88cc108d05975a3ba238761
Daniel Awale <danielmadhukarawale@gmail.com>
2022-12-18 11:26:54 -0800

    contain the seed for the sql

---
+++ server/quotes.sql
@@ -0,0 +1,26 @@
+INSERT INTO quotes (quotes, author) 
+VALUES ('The greatest glory in living lies not in never falling, but in rising every time we fall.', 'Nelson Mandela');
+
+INSERT INTO quotes (quotes, author) 
+VALUES ('The way to get started is to quit talking and begin doing', 'Walt Disney');
+
+INSERT INTO quotes (quotes, author) 
+VALUES ('If life were predictable it would cease to be life, and be without flavor.', 'Eleanor Roosevelt');
+
+INSERT INTO quotes (quotes, author) 
+VALUES ("If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.", 'Eleanor Roosevelt');
+
+INSERT INTO quotes (quotes, author) 
+VALUES ("Life is what happens when you're busy making other plans.", 'John Lennon');
+
+INSERT INTO quotes (quotes, author) 
+VALUES ('Spread love everywhere you go. Let no one ever come to you without leaving happier.', 'Mother Teresa');
+
+INSERT INTO quotes (quotes, author) 
+VALUES ('When you reach the end of your rope, tie a knot in it and hang on.', 'Franklin D. Roosevelt');
+
+INSERT INTO quotes (quotes, author) 
+VALUES ("Don't judge each day by the harvest you reap but by the seeds that you plant." , 'Robert Louis Stevenson');
+
+INSERT INTO quotes (quotes, author) 
+VALUES ('Tell me and I forget. Teach me and I remember. Involve me and I learn.', 'Benjamin Franklin');
