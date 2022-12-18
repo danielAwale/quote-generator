2e35bfe31b51aada62a2d2b395f7d63c4a026a62
Daniel Awale <danielmadhukarawale@gmail.com>
2022-12-18 11:52:08 -0800

    resolve the pool issue, still a few issues left

--- server/index.js
+++ server/index.js
@@ -9,7 +9,8 @@ app.use(express.json());
 app.get("/quotes", async (res, req) => {
     try {
         const allQuotes = await pool.query("SELECT * FROM quotes");
-        res.json(allQuotes.rows);
+        res = allQuotes.rows
+        console.log(res);
     } catch (err) {
         console.log(err.message);
     }
