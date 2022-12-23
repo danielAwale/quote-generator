fa7909bade6a62a2f542c171c40e7cf91544b193
Daniel Awale <danielmadhukarawale@gmail.com>
2022-12-23 14:10:09 -0800

    some progress made

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,29 +4,24 @@ import { useEffect } from 'react';
 const Home = () => {
     const [quotes, setQuotes] = useState([])
 
-    // const quote = quotes[Math.floor(Math.random() * quotes.length)];
+    const generateQuote = (e) => {
+        e.preventDefault();
 
     useEffect(() => {
         fetch('http://localhost:5000/quotes')
         .then(response => response.json())
-        .then(data => setQuotes(data))
+        .then(data => {
+            let quote =  quotes[Math.floor(Math.random() * quotes.length)];
+            setQuotes(data[quote])
+        })
         .catch(error => console.log(error.message))
     },[])
 
-    // const generateQuote = (e) => {
-    //     e.preventDefault();
-    //     return quote;
-    // }
+}
+
   return (
     <>
-    <button>Generate</button>
-
-    {quotes.map((item) => { return (
-        <div>
-            <h3>{item.quotes}</h3>
-            <h5>{item.author}</h5>
-        </div>
-    )})}
+    <button onClick={(e) => generateQuote(e)}>Generate</button>
     </>
   )
 }
