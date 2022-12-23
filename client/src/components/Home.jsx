90ef9f2ed2a61a8b0a2f9055b4f367cfb704f7fe
Daniel Awale <danielmadhukarawale@gmail.com>
2022-12-23 13:36:55 -0800

    useeffect will be used to fetch the data from our backend

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,11 +4,21 @@ import { useEffect } from 'react';
 const Home = () => {
     const [quotes, setQuotes] = useState([])
 
+    useEffect(() => {
+        fetch('http://localhost:5000/quotes')
+        .then(response => response.json())
+        .then(data => setQuotes(data))
+        .catch(error => console.log(error.message))
+    },[])
+
     const generateQuote = (e) => {
         e.preventDefault();
     }
   return (
+    <>
     <button onClick={(e) => generateQuote(e)}>Generate</button>
+    </>
+    
   )
 }
 
