3ed96605b6e6e292b4ecc6d7a44e0d2dedb7df3a
Daniel Awale <danielmadhukarawale@gmail.com>
2022-12-23 22:57:58 -0800

    contain more quotes

--- server/quotes.sql
+++ server/quotes.sql
@@ -24,3 +24,42 @@ VALUES ("Don't judge each day by the harvest you reap but by the seeds that you 
 
 INSERT INTO quotes (quotes, author) 
 VALUES ('Tell me and I forget. Teach me and I remember. Involve me and I learn.', 'Benjamin Franklin');
+
+INSERT INTO quotes (quotes, author) 
+VALUES ('You will face many defeats in life, but never let yourself be defeated.', 'Maya Angelou');
+INSERT INTO quotes (quotes, author) 
+VALUES ('Never let the fear of striking out keep you from playing the game.', 'Babe Ruth');
+INSERT INTO quotes (quotes, author) 
+VALUES ('Life is never fair, and perhaps it is a good thing for most of us that it is not.', 'Oscar Wilde');
+INSERT INTO quotes (quotes, author) 
+VALUES ('The only impossible journey is the one you never begin.', 'Tony Robbins');
+INSERT INTO quotes (quotes, author) 
+VALUES ('Only a life lived for others is a life worthwhile.', 'Albert Einstein');
+INSERT INTO quotes (quotes, author) 
+VALUES ('You only live once, but if you do it right, once is enough.', 'Mae West');
+INSERT INTO quotes (quotes, author) 
+VALUES ('Live in the sunshine, swim the sea, drink the wild air.', 'Ralph Waldo Emerson');
+INSERT INTO quotes (quotes, author) 
+VALUES ('The greatest glory in living lies not in never falling, but in rising every time we fall.', 'Nelson Mandela');
+INSERT INTO quotes (quotes, author) 
+VALUES ('Life is really simple, but we insist on making it complicated.', 'Confucius');
+INSERT INTO quotes (quotes, author) 
+VALUES ('Life is a long lesson in humility.', 'James M. Barrie');
+INSERT INTO quotes (quotes, author) 
+VALUES ('Life is trying things to see if they work.', 'Ray Bradbury');
+INSERT INTO quotes (quotes, author) 
+VALUES ('Life is either a daring adventure or nothing at all.', 'Helen Keller');
+INSERT INTO quotes (quotes, author) 
+VALUES ('You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.', 'Dr. Seuss');
+INSERT INTO quotes (quotes, author) 
+VALUES ('Life is made of ever so many partings welded together.', 'Charles Dickens');
+INSERT INTO quotes (quotes, author) 
+VALUES ('Success is not final; failure is not fatal: It is the courage to continue that counts.', 'Winston S. Churchill');
+INSERT INTO quotes (quotes, author) 
+VALUES ('I find that the harder I work, the more luck I seem to have.', 'Thomas Jefferson');
+INSERT INTO quotes (quotes, author) 
+VALUES ('The secret of success is to do the common thing uncommonly well.', 'John D. Rockefeller');
+INSERT INTO quotes (quotes, author) 
+VALUES ('I never dreamed about success, I worked for it.', 'Estee Lauder');
+INSERT INTO quotes (quotes, author) 
+VALUES ('Success seems to be connected with action. Successful people keep moving. They make mistakes but they do not quit.', 'Conrad Hilton');
