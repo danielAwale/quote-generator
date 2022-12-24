376600c94f403204b5541be070e820108f3b69b9
Daniel Awale <danielmadhukarawale@gmail.com>
2022-12-24 09:15:39 -0800

    contain the button in the middle'

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -29,7 +29,9 @@ const Home = () => {
     <p class="text-slate-200 text-2xl mb-4">
       {quotes.quotes}
     </p>
-    <button type="button" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" onClick={()=> generateQuote()}>Generate</button>
+    <div className='flex justify-center'>
+        <button type="button" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-semibold text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" onClick={()=> generateQuote()}>Generate</button>
+    </div>
   </div>
 </div>
 
