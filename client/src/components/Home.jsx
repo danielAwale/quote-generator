4a41a0947bd011d7dc4df0fc0658ee8a3b050fd5
Daniel Awale <danielmadhukarawale@gmail.com>
2022-12-23 23:38:16 -0800

    play with styles

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -23,10 +23,10 @@ const Home = () => {
   return (
     <>
 
-<div class="flex justify-center">
-  <div class="block p-6 rounded-lg shadow-lg max-w-3xl item-center">
-    <h5 class="text-slate-400 text-4xl leading-tight font-medium mb-2">{quotes.author}</h5>
-    <p class="text-slate-300 text-2xl mb-4">
+<div class="flex justify-center item-center">
+  <div class="rounded-lg shadow-lg max-w-3xl item-center">
+    <h5 class="text-slate-300 text-4xl leading-tight font-medium mb-2">{quotes.author}</h5>
+    <p class="text-slate-200 text-2xl mb-4">
       {quotes.quotes}
     </p>
     <button type="button" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" onClick={()=> generateQuote()}>Generate</button>
